eb4003ed1b9870a59ccf8ad9f375b427573ebc13
wangheng <1070842676@qq.com>
2018-08-08 22:05:49 +0800

    1第二次

---
+++ src/main/java/rest.java
@@ -0,0 +1,9 @@
+/**
+ * Created by wh on 2018/8/8.
+ */
+public class rest {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+        System.out.println("weqewqeq");
+    }
+}
